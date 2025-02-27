334d3c8ca635440fe85886d1da532d5b8d486e29
lalitcap23 <lalitrajput232002@gmail.com>
2025-02-27 22:08:56 +0530

    zk

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_GRAPHQL_ENDPOINT: string;
+        NEXT_PUBLIC_CHAIN_ID: string;
+        NEXT_PUBLIC_SCHEMA_URL: string;
+    }
+}
